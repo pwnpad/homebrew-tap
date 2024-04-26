85571adc8d52764920f08268abebbddc41125dea
Platy <daryl.limkz@gmail.com>
2024-04-26 19:10:47 +0800

    Bump to v1.2.1

--- pwnpad.rb
+++ pwnpad.rb
@@ -1,10 +1,10 @@
 class Pwnpad < Formula
   desc "🐳 VMs are bloat. Dockerise your CTF environment."
   homepage "https://hub.docker.com/r/platypew/pwnpad"
-  url "https://github.com/pwnpad/pwnpad/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "ec76d0537756a418d5902e741ed23f60979ae1a8aa00c9719d2cf6a4b64a6a62"
+  url "https://github.com/pwnpad/pwnpad/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "02f219e97f4a989124b4f8fda650599a51c7e192af749272601c52236c819e38"
   license "MIT"
-  version "1.1.3"
+  version "1.2.1"
 
   depends_on "docker" => :optional
 
